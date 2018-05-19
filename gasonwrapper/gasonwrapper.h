39e76907aedbf4a058782c865a6add27d0c41958
Benjamin <orbenappdev@gmail.com>
2018-05-19 11:39:08 +0800

    Initial Commit

---
+++ gasonwrapper/gasonwrapper.h
@@ -0,0 +1,19 @@
+//
+//  gasonwrapper.h
+//  gasonwrapper
+//
+//  Created by Benjamin on 19/5/2018.
+//  Copyright © 2018 Benjamin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for gasonwrapper.
+FOUNDATION_EXPORT double gasonwrapperVersionNumber;
+
+//! Project version string for gasonwrapper.
+FOUNDATION_EXPORT const unsigned char gasonwrapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <gasonwrapper/PublicHeader.h>
+
+
